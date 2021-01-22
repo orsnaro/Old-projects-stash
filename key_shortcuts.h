6044c19620635a202558d28c5f7bba41457dfc8c
omar rashad salem <77698890+orsnaro@users.noreply.github.com>
2021-01-22 23:04:53 +0200

    edit 2

--- key_shortcuts.h
+++ key_shortcuts.h
@@ -6,5 +6,6 @@
 'ctrl+y' redo last undo
 'ctrl+tap+left or right' to delete word at once
 'ctrl+scroll' zoom in or out
-
+'ctrl+shift+p' open  kebindings in Atom Editor
+'ctrl+,' open settings in Atom editor
 select same word  or edit same word only irepl it T_T
