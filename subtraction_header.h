d91d2e9fd6b949b0b2658ce5bc06b7050f180935
sad <dad>
2021-01-27 03:32:20 +0200

    add comments

--- subtraction_header.h
+++ subtraction_header.h
@@ -1,11 +1,9 @@
 
 
 //this belongs to ors simple calculator.
-
+// حطيتها لوحدها فقط لكي اقوم بتطبيق عدة طرق في تنفيذ وندا الدوال ! :P
 float subtraction(float x,float y)
 {
 float res=x-y;
 return(res);
 }
-
-
