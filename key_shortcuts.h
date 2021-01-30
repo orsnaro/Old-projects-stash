effc14aa6dd0b132da0eb9b4e7e512ee8a7d1252
sad <dad>
2021-01-31 00:59:25 +0200

    d

--- key_shortcuts.h
+++ key_shortcuts.h
@@ -9,7 +9,9 @@
 'ctrl+scroll' zoom in or out
 'ctrl+shift+p' open  kebindings in Atom Editor
 'ctrl+,' open settings in Atom editor
-'Alt+F3' select all same word  (atom shortcut)
+'Alt+F3' select all same word  (ATOM shortcut)
+'Alt+d' select only same next word (ATOM)
+'ctrl+f' find and replace (ATOM)
 select same word  or edit same word (not in codelite)  T_T
 very usefull explanation for c functions at : https://www.ibm.com/support/knowledgecenter/ssw_ibm_i_74/rtref/scanf.htm
 
