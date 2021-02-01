b10cd2e19ac9387acabaeaf7f991e2a4077bf533
sad <dad>
2021-02-01 02:35:08 +0200

    f

--- TEST.c
+++ TEST.c
@@ -2,11 +2,20 @@
 
 //we can add "windows.h" header if we want to use Sleep(ms) 'captial S'.
  int x=4,y=4 ; //global
+ float mat_multiply( float A[x][y], float B[x][y],float C[x][y]);
+ float mat_multiply( float A[x][y], float B[x][y],float C[x][y]);
 
  int main()
  {
+   static float Z[4][4],W[4][4],K[4][4];
 
-
+float mat_sub( Z,W,K);
   return 0;}
+
+
+
+
+
+
 
   //stoped here ! 31-1-2021
