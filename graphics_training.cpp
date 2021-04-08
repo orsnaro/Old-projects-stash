6e01a73e47b0a2a22ed2b3aa676c91d5289d52f8
sad <dad>
2021-04-08 17:46:37 +0200

    Update graphics_training.cpp
    finally  graphics.h working

--- graphics_training.cpp
+++ graphics_training.cpp
@@ -1,1 +1,18 @@
-sdfasdfsd
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <graphics.h>
+#include <conio.h>
+
+#include <direct.h>
+
+int main()
+{
+  int gd= DETECT , gm , errorcode;
+  initgraph(&gd, &gm, (char*) " " );
+  getch();
+  closegraph();
+
+
+  return 0;
+}
