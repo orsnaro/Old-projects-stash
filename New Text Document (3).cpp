444dc6377d4d3b56315e5e04c61377705b2e1cbe
omar rashad salem <77698890+orsnaro@users.noreply.github.com>
2021-08-15 01:06:03 +0200

    Add files via upload

---
+++ New Text Document (3).cpp
@@ -0,0 +1,33 @@
+/SPECIALLY FOR CODELITE EDITE+OR (IDE)
+'ctrl+up\down' to swap lines places
+'end' to go to the end of the line
+'home' to go to the begining of line
+'ctrl+tap' to switch between taps
+'ctrl+y' redo last undo
+'ctrl+tap+left or right' to delete word at once
+'ctrl+scroll' zoom in or ou
+'ctrl+shift+p' open  kebindings in Atom Editor
+'ctrl+,' open settings in Atom editor
+'Alt+F3' select only next same word  (ATOM shortcut)
+'Alt+d' select all same next word (ATOM)
+'ctrl+f' find and replace (ATOM)
+select same word  or edit same word (not in codelite)  T_T
+very usefull explanation for c functions at : https://www.ibm.com/support/knowledgecenter/ssw_ibm_i_74/rtref/scanf.htm
+
+======> specialy for ATOM  shortcuts https://github.com/nwinkler/atom-keyboard-shortcuts
+for ECLIPSE =====> ctrl+ shft+L (see keybindings)
+
+
+(FOR GIT)
+…or create a new repository on the command line
+echo "# study_compatitive_oop_cpp" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/orsnaro/study_compatitive_oop_cpp.git
+git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/orsnaro/study_compatitive_oop_cpp.git
+git branch -M main
+git push -u origin main
