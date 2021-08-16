e58d417eb17115b32f7943ed7affee4a38a73082
orsnaro <omar1xd@gmail.com>
2021-08-17 00:28:09 +0200

    finished s5 hw5 14# +update shortcut

---
+++ cpp4kids_s5_ep12_17.cpp
@@ -0,0 +1,112 @@
+//============================================================================
+// Name        : cpp4kids_s5....
+// Author      : Omar.R.S
+// Version     :
+// Copyright   : Your copyright notice
+// Description : solving cpp4kids H.W (while\for loops) C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+//h.w1 14#
+
+	/*
+
+	 int a,b,i;
+	 cin>>a>>b>>i;
+	 i=a;
+
+	 while(i<=b){
+	 cout<<i<<"\n";
+	 i++;
+	 }
+
+	 */
+
+
+
+
+
+
+//============================================================================
+//h.w 3,4,5 14#
+
+	/*
+
+	int N, taps, stars;
+
+	cin >> N;
+	static int i, j;
+
+	while (i < N) {
+
+		stars = 1 + 2 * i;
+		taps = N - (i + 1); //try put this two eqn outside while and see if they update value :D.
+
+			while (j <= taps) {
+				cout << " ";
+				j++;
+			}
+
+
+		j = 0;
+		while (j < stars) {
+			cout << '*';
+			j++;
+		}
+		cout << "\n";
+		j = 0;
+		i++;
+	}
+
+	j = 0;
+	i = N - 1;
+	while (i >= 0) {
+
+		stars = 1 + 2 * i;
+		taps = N - (i + 1);
+
+		while (j <= taps) {
+			cout << " ";
+			j++;
+		}
+
+		j = 0;
+		while (j < stars) {
+			cout << '*';
+			j++;
+		}
+
+		cout << "\n";
+		j = 0;
+		i--;
+	}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//============================================================================
+	//h.w7 14#
+
+
+
+
+	return 0;
+}
