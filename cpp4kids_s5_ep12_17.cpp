0374d8ef18f4e3c7e258283af408d4eca492b680
orsnaro <omar1xd@gmail.com>
2021-08-18 03:41:24 +0200

    s
    finished s5 to hw11 14#

--- cpp4kids_s5_ep12_17.cpp
+++ cpp4kids_s5_ep12_17.cpp
@@ -85,25 +85,88 @@ int main() {
 		i--;
 	}
 */
+//============================================================================
+//h.w7 14#
+	/*
+	 static int n, i;
+
+	 cin >> n;
+
+	 if (n < 0 || n > 200) {
+	 cout << "error ,retry";
+	 return 0;
+	 }
+	 while (i <= n) {
+	 i++;
+	 if (i % 8 == 0 || (i % 4 == 0 && i % 3 == 0))
+	 cout << i << "\t";
+	 }
+	 */
+	//---------------------------------------------------------------------------------
+	//h.w9 cpp4kids 14#
+	/*
+	string no_dtct;
+	static int i, n;
+
+	cin >> n;
+	if (n <= 0) {
+		cout << "error ,retry";
+		return 0;
+	}
+
+	while (i < n) {
+
+		cin >> no_dtct;
+
+		if (no_dtct == "no" || no_dtct == "No" || no_dtct == "nO"
+				|| no_dtct == "NO")
+			cout << no_dtct << " ";
+		i++;
+	}
+*/
+
+	//---------------------------------------------------------------------------------
+	//h.w11 cpp4kids 14#
+
+	/*
+	int min, cases, num_cntr, num, i, j;
+	cin >> cases;
+
+	i = 0;
+	j = 0;
+
+	while (i < cases) {
+		i++;
+		cin >> num_cntr;
+		if (num_cntr == 0) {
+			cout << 0;
+
+			continue;
+		}
+
+		cin>>num;
+		min = num;
+
+		while (j < num_cntr - 1) {
+
+			cin >> num;
+			if (min > num)
+
+				min = num;
+			j++;
+		}
+
+		cout << min << endl;
+		j = 0;
+
+	}
+	*/
+
+	//---------------------------------------------------------------------------------
+					//h.w131 cpp4kids 14#
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//============================================================================
-	//h.w7 14#
 
 
 
