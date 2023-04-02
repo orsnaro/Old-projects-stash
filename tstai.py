55b39391866ee7212f7722056afa899ea99e9964
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-04-03 00:54:56 +0200

    # moved ai notes to separate text file  + some progress in tstai.py #

--- tstai.py
+++ tstai.py
@@ -1,158 +1,75 @@
 
 import cv2
+import numpy as np
 import pytesseract as tsr
-# 	# TODO : the car_plate reader function
-# 	# TODO : the simple id card reader function
-# 	# TODO : feature matching to detect if this not and id and retry
 
-# vid = cv2.VideoCapture(0)
-# while (1) :
-# 	ret , frame =  vid.read()
-
-# 	cv2.imwrite("test.jpg" , frame)
-# 	cv2.imshow("frame", frame)
-
-
-# 	if cv2.waitKey(1) & 0xff == ord('q'):
-# 		break
-
-# vid.release()
-# cv2.destroyAllWindows()
-
-img = cv2.imread(r"./test_id_yousef_salah.jpg", 1)
-img = cv2.resize(img, (600, 400))
-img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1] # returns  (thresh type , threshed_image)
-imgstr = tsr.image_to_string(img, lang='eng')
-# for loop to check isnumeric() and only save numeric char then join them in one string
-imgstr = imgstr.split()
-print(imgstr[1])
-
-
-def car_plate_reader(): ...
-
-
-def simple_id_reader(): ...
-# open video camera
-# capture frame from camera
-# gaussian blur the image
-# threshold the image ( binary or smart threshold)
-# ocr the result image
-# return text ( id )
-
-
-if __name__ == "__main__":
-	 ...
-	 # your test code
-
-""" help text for later  ( better read it at telegram 'test chat')
- Sure, I can help you with that!
-
-OCR (Optical Character Recognition) is a technology that enables computers to read text from images. Pytesseract is a popular OCR library for Python, and it can be used to detect ID card numbers from images captured by a laptop camera.
-
-Here's how you can do it:
-
-Install the necessary libraries:
-You'll need to install the following libraries:
-
-Pytesseract: pip install pytesseract
-OpenCV: pip install opencv-python
-Capture the image:
-You can use the OpenCV library to capture an image from your laptop camera. Here's an example code:
-
-python
-import cv2
-
-camera = cv2.VideoCapture(0)
-_, image = camera.read()
-cv2.imwrite('id_card_image.jpg', image)
-camera.release()
-
-This code captures an image from the laptop camera and saves it as 'id_card_image.jpg'.
-
-Process the image:
-Before running OCR on the image, you may need to preprocess it. For example, you can apply image filtering and thresholding to make the text stand out more clearly. Here's an example code:
-
-python
-
-import cv2
-import pytesseract
-
-# Read the image
-image = cv2.imread('id_card_image.jpg')
-
-# Convert to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Apply thresholding
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-# Apply median blur to remove noise
-median_blur = cv2.medianBlur(thresh, 3)
-
-# Apply dilation to make the text thicker
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-dilated = cv2.dilate(median_blur, kernel, iterations=1)
-
-# Run OCR on the processed image
-result = pytesseract.image_to_string(dilated)
-
-# Print the detected text
-print(result)
-This code reads the image, converts it to grayscale, applies thresholding and median blur, and then dilates the text to make it thicker. Finally, it runs OCR on the processed image and prints the detected text.
-
-Note that the image_to_string function of pytesseract returns all the text detected in the image, so you may need to extract the ID card number from the result using regular expressions or other text processing techniques.
-
-I hope this helps! Let me know if you have any questions.
+# TODO : the car_plate reader function
+# TODO : the simple id card reader function
+# TODO : feature and object  matching to detect if this is not an id and retry
+# if second time failed add option in gui to enter id manually 
+# #(if we could add auto id complete in input would be nice )
+# TODO : change stop video by key press to fixed time shown by a timer
 
 
 
-
-*f98bf0d80c@boxmail.lol
-now complete the example using pytesseract*
-
-Sure, here's a complete example using pytesseract to extract the ID card number from an image captured by a laptop camera:
-
-python
-
-import cv2
-import pytesseract
-import re
-
-# Set the path to the pytesseract executable
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-# Capture the image from the laptop camera
-camera = cv2.VideoCapture(0)
-_, image = camera.read()
-cv2.imwrite('id_card_image.jpg', image)
-camera.release()
-
-# Read the image
-image = cv2.imread('id_card_image.jpg')
-
-# Convert to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Apply thresholding
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-# Apply median blur to remove noise
-median_blur = cv2.medianBlur(thresh, 3)
-
-# Apply dilation to make the text thicker
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-dilated = cv2.dilate(median_blur, kernel, iterations=1)
-
-# Run OCR on the processed image
-result = pytesseract.image_to_string(dilated)
-
-# Extract the ID card number using regular expressions
-id_card_number = re.search(r'[A-Z]{2}\d{8}', result).group()
-
-# Print the ID card number
-print('ID card number:', id_card_number)
-This code captures an image from the laptop camera, processes it using various image processing techniques, runs OCR using pytesseract, and then extracts the ID card number using regular expressions. Finally, it prints the extracted ID card number.
-
-Note that you'll need to modify the path to the pytesseract executable to match your system configuration. Also, the regular expression used to extract the ID card number assumes that it consists of two uppercase letters followed by eight digits. You may need to adjust it to match the format of the ID card number in your country.
+def simple_card_reader ( id_dimension : tuple = (800 , 400) ) -> str :
+	"""
+	this function does following (until now):
+	1. start video capture
+	2. provide rectangle at video center to help user scan id
+	3. save camera frame when 'q' key is pressed 
+	4.proccess taken frame for better ocr ( threshold , smooth , dialate) 
+	5. ocr 
+	6. parse the text to take only id for now ( 14 numeric digits )
+	7. return the ocr(ed) ID carn number
  
-"""
+	Default id shape is : ( 800 x 400) (x,y)
+	"""
+	#make capture object and pass camera index
+	vid = cv2.VideoCapture(0)
+
+	#rect obj specs
+	color = (0,255,0) #green color in BGR format
+	thickness = 2 
+	top_left_coordinate : tuple #(x,y) top left most corner is origin (0,0)
+	bot_right_coordinate : tuple
+
+
+	while (1) : #capture camera at 100fps
+		
+		is_error , frame =  vid.read()
+		cv2.imwrite("test.jpg" , frame)
+	
+	
+		#make rectangle object ot position ID card
+		cv2.rectangle(frame , )
+	
+		#center the rectangle in video frame
+		
+		#show live video ( user visual aid)
+		cv2.imshow("Camera", frame) 
+
+
+		if cv2.waitKey(10) & 0xff == ord('q'):
+			break
+
+	vid.release()
+	cv2.destroyAllWindows()
+
+	img = cv2.imread(r"./test_id_yousef_salah.jpg", cv2.IMREAD_GRAYSCALE)
+	# img = cv2.resize(img, (600, 400))
+	img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1] # returns  (thresh type , threshed_image)
+	imgstr = tsr.image_to_string(img, lang='eng')
+	# for loop to check isnumeric() and only save numeric char then join them in one string
+	imgstr = imgstr.split()
+	print(imgstr[1])
+
+
+
+	return id 
+
+
+
+
+if __name__ == "__main__": ...
+	 # your test code
