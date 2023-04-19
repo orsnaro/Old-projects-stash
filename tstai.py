1ad8859eb8eda61bb1adff5cf3f3c98a368ff1e9
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-04-19 17:53:11 +0200

    # done basic de skew via matching ORB algo # enhanced de skew coming later #

--- tstai.py
+++ tstai.py
@@ -1,14 +1,16 @@
 """
-								  Coder : Omar
-								  Version : v---
-								  Date :  9 / 4 / 2023
+								  Coder : Eng.Omar
+								  Version : v1.0B
+								  version Date :  19 / 4 / 2023
 								  Code Type :  CV | OCR => smart_parking_project
 								  Title : Smart Parking System
 								  Interpreter : cPython  v3.11.0 [Compiler : MSC v.1933 AMD64]
 """
 
+import os 
+import sys
 import cv2
-# import os 
+import pyautogui
 import numpy as np
 import pytesseract as tsr
 import garage_db as db
@@ -18,7 +20,9 @@ import garage_db as db
 # TODO : show a  timer in  video ( 5 ~ 10 sec )  use cv2.puttext()
 # TODO : add audio feedback 
 # TODO : car plate / license id simple  template detection
-# TODO : feature and object  matching/tracking 
+# TODO : tracking 
+# TODO : if current de-skew and matching . algo doesnt work try SIFT or even brute force rotation 5 deg by 5 dgree
+# TODO : make it work with real id cards ( mostly will change tesseract psm mode to parse )
  #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
 
 
@@ -128,9 +132,10 @@ def enable_multithreading () : ... #in case gpu acceleration is not enough to ma
  #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
 
  #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
-def video_settings_setup (cam_indx : int  = 0, fps : int = 30 , vid_time_sec : int = 20, res : tuple = (1366 , 768)) -> tuple :
+def video_settings_setup (cam_indx : int  = 0, fps : int = 30 , vid_length_sec : int = 10, res : tuple = (1366 , 768)) -> tuple :
 	"""
 	* This function does set up the video objects
+	* res is set by default to 720p
  
 	* NOTE 1:
  
@@ -146,7 +151,7 @@ def video_settings_setup (cam_indx : int  = 0, fps : int = 30 , vid_time_sec : i
   
 	* Returns :
  
-		vid , fps , frametime , vid_time_sec , active_gpu_api
+		vid , fps , frametime , vid_length_sec , active_gpu_api
   
 
 	"""
@@ -167,16 +172,148 @@ def video_settings_setup (cam_indx : int  = 0, fps : int = 30 , vid_time_sec : i
 	else : #DEFAULT 
 		vid = cv2.VideoCapture(cam_indx , cv2.CAP_DSHOW)
   
-	#set custom res
-	vid.set(cv2.CAP_PROP_FRAME_WIDTH , res[0]) 
-	vid.set(cv2.CAP_PROP_FRAME_HEIGHT, res[1])
+	#TO GET YOUR DEFAULT CV2 RES :
    # default res on omarpc obs vritual cam plugin -> (height_Y : 480 , width_X : 640)
+	# hCV2 = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)#TESTING
+	# wCV2 = vid.get(cv2.CAP_PROP_FRAME_WIDTH)#TESTING
+	# print (f"cv2 res is : {wCV2, hCV2}") #TESTING
+	#TO GET YOUR DEFAULT SCREEN RES :
+	# wSCREEN , hSCREEN = pyautogui.size()#TESTING
+	# print ( f" screen res is : {wSCREEN , hSCREEN}")#TESTING
+		  
+	
+	#set custom res
+	vid.set(cv2.CAP_PROP_FRAME_WIDTH , res[0] ) 
+	vid.set(cv2.CAP_PROP_FRAME_HEIGHT, res[1] )
 	
  
 	#frametime is needed to put as cv2.waitkey() argument 
 	frametime = 1000 // fps 
 
-	return vid , fps , frametime , vid_time_sec , gpu_api 
+	return vid , fps , frametime , vid_length_sec , gpu_api 
+ #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+ 
+ #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+
+def compare_img ( img_to_comp : np.ndarray ) -> list :
+	''' Returns : good_matches , (ref_kp , img_kp) '''
+	ref_img = get_ref_img_db()
+
+#NOTE: DETECT keypoints and create the decriptors for ref.img and img_to_cmp
+
+	detect_obj = cv2.ORB_create() #2 algorithms in ORB : corner detection and binary descriptor extraction 
+	#instead of ORB you could use SIFT. it's better and more accurate but slower.
+	#since our objects are relativily simple and we need high process rate (30FPS+) will use ORB for now.
+
+	#brute force matching + norm_hamming (is better for cv and images than L1 , L2)
+	match_obj = cv2.BFMatcher(cv2.NORM_HAMMING) 
+
+	ref_kp , ref_desc = detect_obj.detectAndCompute(ref_img , mask= None)
+	img_kp , img_desc = detect_obj.detectAndCompute(img_to_comp , mask= None)
+
+#NOTE: MATCH and save best matches
+	matched_descs = match_obj.match(ref_desc , img_desc) 
+	#match() returns DMatch_Obj = [img_desc_indx , ref_img_desc_indx , distance]
+	
+	matched_descs = sorted(matched_descs , key= lambda x : x.distance) # each x is a matched_descs obj
+	
+	good_matches = matched_descs[:20] #get only best 10 matches (smallest distance)
+	print (f"tot number of matched descs: {len(matched_descs)}") #TESTING
+	
+	return ( good_matches , (ref_kp , img_kp) )
+ 
+ #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+def get_trans_mat( good_matches : cv2.DMatch  , key_points : tuple ) -> np.ndarray :
+	''' Returns : homography transformation matrix '''
+ 
+	#NOTE:  homography transformation matrix : 
+	# [ cos(theta)  -sin(theta)  0 ]
+	# [ sin(theta)   cos(theta)  0 ]
+	# [     0            0       1 ]
+	#trans. mat will be pure rotation mat (like above) 
+	# if and ONLY if the image is only rotated  with no any other transformation
+	ref_kp , img_kp = key_points
+
+	ref_good_pts = np.float32( [ref_kp[m.queryIdx].pt for m in good_matches] ) # 'pt' is the pixel coordinate of a keypoint
+	ref_good_pts = ref_good_pts.reshape(-1,1,2) #3D mat of coordintes [ [x1,y1] , [x2,y2] ..]
+ 
+	img_good_pts = np.float32( [img_kp[m.trainIdx].pt for m in good_matches] )
+	img_good_pts = img_good_pts.reshape(-1,1,2)
+ 
+ 	#in finHomography() func: cv2.RANSAC is a (match outlier excluder Algorithm) and 5.0 is the thresh
+	trans_mat , choosed_matches_mask = cv2.findHomography(  ref_good_pts , img_good_pts , cv2.RANSAC , 5.0 ) 
+	
+	return trans_mat 
+
+ #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+def get_skew_angle( homograph_rot_mat : np.ndarray , img_to_skew_shape : tuple ) -> tuple:
+	"""
+#### Pure rotation mat should look like :
+  
+|  [ cos(theta) | -sin(theta)| 0 ]  	|
+| ------- 		 | --- 		  | ---     |
+|  [ sin(theta) | cos(theta) | 0 ] 		|
+|  [     0 		 | 0 			  | 1 ]		|
+
+---
+---
+	Returns:
+ 
+		( skew_angle , (center , h , w) )
+  
+  if fail return -1 , -1
+	"""
+	if type (homograph_rot_mat) != type(None) :
+		sine = homograph_rot_mat [1 , 0]
+		cos  = homograph_rot_mat [0 , 0]
+		skew_angle = np.arctan2(sine , cos) * (180 / np.pi)	
+		print ( f"IMAGE ROTATION ANGLE IS : {skew_angle} DEGREES")#TESTING
+	else :
+		return -1 , -1 #skip this frame
+	
+	h , w = img_to_skew_shape
+	center = ( w // 2 , h // 2)
+	
+	return ( skew_angle , (center , w , h) )
+ #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+def affine_trans ( _skew_angle : int , img_coord : tuple , _image_to_skew : np.ndarray) -> np.ndarray :
+	"""
+	Returns:
+		 deskewed_image : np.ndarray
+	"""
+	center , w , h = 	img_coord
+ 
+	affine_rot_mat = cv2.getRotationMatrix2D(center , _skew_angle , scale= 1.0) #1.0 is image scale factor
+	rotated_img = cv2.warpAffine(_image_to_skew , affine_rot_mat , (w,h) , flags= cv2.INTER_CUBIC, borderMode= cv2.BORDER_REPLICATE )
+	deskewed_img = rotated_img
+ 
+	return deskewed_img
+ #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+def rotate_180 (img_to_rotate_180 : np.ndarray) -> np.ndarray :
+	rot_angle = 180
+	rotated_180_img = affine_trans (rot_angle , img_to_rotate_180)
+
+	return  rotated_180_img
+ #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+def deskew_img(  img_to_skew : np.ndarray ) -> np.ndarray : #lets call it de-rotate for now
+	"""
+
+	Returns:
+		 np.ndarray: image after deskew
+   if error return -1
+	"""
+	img_shape = img_to_skew.shape	
+ 
+	good_matches , key_points  = compare_img( img_to_skew )
+	trans_mat 	 					= get_trans_mat( good_matches , key_points)
+	skewed_angle , coordinates = get_skew_angle ( trans_mat , img_shape)
+ 
+	if skewed_angle == -1 or coordinates == -1 : 
+		return -1
+	else : 
+		deskewed_img = affine_trans( skewed_angle , coordinates , img_to_skew)
+		return deskewed_img 
+
  #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
  
  #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
@@ -189,6 +326,8 @@ def proccess_vid_frame( _frame : np.ndarray , _id_dimension : tuple , _is_valid 
 
 		Returns:
 			final_img
+   
+		if error return -1
 	"""
   
 		#original video frame with out the rectangle and timer is what we will process
@@ -205,31 +344,66 @@ def proccess_vid_frame( _frame : np.ndarray , _id_dimension : tuple , _is_valid 
 		img = img[ y1 : y2 , x1 : x2  ]
 
 		#change image to gray scal cuz THRESH OTSU Needs that
-		img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
-
-		#cv2.threshold() -> returns  (thresh type , threshed_image)
-		img_threshed  = cv2.threshold( img , 0 , 255, cv2.THRESH_BINARY |  cv2.THRESH_OTSU  )[1] 
-		#NOTE : search why they use binary with otsu threshold hold not otsu only 
-
-		# median blur to remove noise
-		# img_threshed_blured = cv2.medianBlur(img_threshed, 3) 
-		
-		#TODO : sharpen image after removing noise using laplacian 
   
-		#TODO : deskew 
+	#TESTING BLOCK
 
-		#TODO : read about then try diff morphological operation or combine them (dialation / erosion / opening)
-		# dilation to make the text thicker
-  	# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3)) #bigger kernel usually fits smaller fonts
-		# img_final = cv2.erode(img_threshed, kernel, iterations=1)
-		img_final = img_threshed
-		# use cv2.dilate() instead if you want to expand the white pixels 
-		# (look up dilation and erosion)
+		img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
+		cv2.imshow("TESTING : show image before edit *grayed*" , img)
+  
+		img = cv2.bitwise_not(img)
+		cv2.imshow("TESTING : show image bitwise not" , img)
+  
+  
+		img = cv2.threshold(img , 0 , 255 , cv2.THRESH_BINARY_INV+ cv2.THRESH_OTSU)[1]
+		cv2.imshow("TESTING : show image threshed" , img)
 
-		# TESTING BLOCK #
-		cv2.imshow("TESTING : show image before edit" , img)
-		cv2.imshow("TESTING : show image after edit" , img_final)
-		# cv2.destroyAllWindows()
+		img = deskew_img(  img  )
+  
+		if type(img) == type(-1) : #skip this frame
+			return -1			
+
+		#continue process
+		img = cv2.Laplacian(img, cv2.CV_8U, ksize=3)
+		cv2.imshow("TESTING : show image sharpened" , img)
+  
+  
+		kernel = np.ones((1,3) , dtype= np.uint8)#little bit  better for text(more horizentally focused)
+		kernel2 = np.ones((3,3) , dtype= np.uint8)
+		img = cv2.dilate(img , kernel= kernel)
+  
+  
+
+
+		img_final = img
+		cv2.imshow("TESTING : show image final)" , img_final)
+	#TESTING BLOCK
+  
+  
+  
+  #cv2.threshold() -> returns  (thresh type , threshed_image)
+		# img_threshed  = cv2.threshold( img , 0 , 255, cv2.THRESH_BINARY |  cv2.THRESH_OTSU  )[1] 
+		# #NOTE : search why they use binary with otsu threshold hold not otsu only 
+
+		# # median blur to remove noise
+		# # img_threshed_blured = cv2.medianBlur(img_threshed, 3) 
+		
+		# #TODO : sharpen image after removing noise using laplacian 
+  
+		# #TODO : deskew 
+
+		# #TODO : read about then try diff morpholog ical operation or combine them (dialation / erosion / opening)
+  
+		# # dilation to make the text thicker
+  		# # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3)) #bigger kernel usually fits smaller fonts
+		# # img_final = cv2.erode(img_threshed, kernel, iterations=1)
+		# img_final = img_threshed
+		# # use cv2.dilate() instead if you want to expand the white pixels 
+		# # (look up dilation and erosion)
+
+		# # TESTING BLOCK #
+		# cv2.imshow("TESTING : show image before edit" , img)
+		# cv2.imshow("TESTING : show image after edit" , img_final)
+		# # cv2.destroyAllWindows()
 
 		# END OF TESTING BLOCK #
 
@@ -314,7 +488,7 @@ def read_simple_card_opencl( vid : cv2.VideoCapture , vid_specs : list , id_card
 
 		vid
 
-		vid_specs : fps , frametime(1000 ms / fps) , vid_time_sec , active_gpu_api
+		vid_specs : fps , frametime(1000 ms / fps) , vid_length_sec , active_gpu_api
 
 		id_dimension
 
@@ -329,22 +503,28 @@ def read_simple_card_opencl( vid : cv2.VideoCapture , vid_specs : list , id_card
 	status is 'False' when error reading frame or scan id
 	"""
 	valid_cnt = 0
-	valid_ids_freq = {}
+	valid_ids_freq = {} #updated as search_id()
 	vid_time_cnt = vid_specs[2]
 	id_dimension = id_card_specs['dimension']
 	while vid_time_cnt > 0: #start capture camera at fps for 10seconds
 
 		no_error , frame =  vid.read()
 
-		if not no_error :
-			return False , None #Fail error reading frame
-	
-
-		
+		if  not no_error :
+			return False , "Fail error reading frame" #Fetal Fail error reading frame
   
 		img_final = proccess_vid_frame(frame , _id_dimension= id_dimension , _is_valid= is_valid)
+		if type(img_final) == type(-1) : #skip this frame
+			print(f"""
+         warninig!: this frame : {vid_time_cnt} will be skipped from ocr and id db check  
+         
+         due to cv2 unable to generate  homopraghy transformation mat""")
+			continue
+
+		#custom tesser configuration if needed
+		cfg = "--psm 11 --oem 3" # Sparse text. Find as much text as possible in no particular order.
   
-		imgstr = tsr.image_to_string(img_final, lang='eng')
+		imgstr = tsr.image_to_string(img_final, lang='eng' , config= cfg )
   
 		is_valid, *_ = search_id ( id_card_specs['id_char'] , imgstr , valid_ids_freq) #edits valid_ids_freq inside
   
@@ -359,23 +539,61 @@ def read_simple_card_opencl( vid : cv2.VideoCapture , vid_specs : list , id_card
 
 		fps = vid_specs[0]
 		vid_time_cnt -= 1
-		if valid_cnt >= fps :  #if still valid for whole one sec accepted 
+		validate_after= fps  #after 1 sec 
+  
+		if valid_cnt >= validate_after:  #perfect match  if still valid for whole validate_after (sec)
 			all_success = True
 			#get only one id -> the higest freq ( if two has same freq choose the first to be inputed in freq_arr)
 			final_value = max ( valid_ids_freq ,  key= valid_ids_freq.get ) 
 			return all_success , final_value 
+
+
 		
 		frametime = vid_specs[1]
 		cv2.waitKey( frametime )  
 
-	return False , None #fail
+	if valid_cnt > validate_after // 2  :
+		return True , max ( valid_ids_freq ,  key= valid_ids_freq.get ) #Sucess but not perfect match
+
+	#TODO :  if image is +-180 degree rotated 
+ 	# elif not_rotated_yet : ... #use recursion
+ 
+	else :
+		return False , max ( valid_ids_freq ,  key= valid_ids_freq.get ) #fail but return best guess
  #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
 
-
-
-
  #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
-def ocr_main (id_dimension : tuple = (405 , 271) , id_type_indx : int = 0 ) -> str : 
+
+def save_ref_img_db (): #only use manually
+
+	#Apply same operations that made to image before calling deskew()
+	img_path = "clear.jpg"
+	path_no_ext , img_format = os.path.splitext(img_path)
+	ref_img = cv2.imread(f"./{img_path}", cv2.IMREAD_GRAYSCALE)
+	ref_img = cv2.bitwise_not(ref_img)
+	ref_img = cv2.threshold(ref_img , 0 , 255 , cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+	#encode in bytes to compress 
+	success , ref_img_encoded = cv2.imencode( ext= img_format , img= ref_img )
+ 
+	if not success :
+		print("Error : Encoding image failed in save_ref_img_db()  exiting(1)>>")
+		sys.exit( 1 ) #fail and raise sys excpetion
+  
+	else:
+		#send to db
+		db.access_img_table( 1 , img_to_write= ref_img_encoded )
+ #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+def get_ref_img_db(img_name : str = "ref_rot_img") -> np.ndarray :
+   
+	ref_img_encoded =  db.access_img_table( 0  , img_name= img_name )
+	ref_img = cv2.imdecode( np.frombuffer(ref_img_encoded , dtype= np.uint8) , cv2.IMREAD_GRAYSCALE)
+ 
+	return ref_img
+ #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+ 
+ #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+def ocr_main (id_dimension : tuple = (810 , 542) , id_type_indx : int = 0 ) -> str : 
 	"""
 	* this function does following (until now):
 
@@ -396,14 +614,14 @@ def ocr_main (id_dimension : tuple = (405 , 271) , id_type_indx : int = 0 ) -> s
 
 		7. return the ocr(ed) ID card number
 
-	* Default id shape is : ( 405 x 271 ) (x,y) (later this will ocr car plates and license)
+	* Default id shape is : ( 810 x 542 ) (x,y) (later this will ocr car plates and license)
 	actual size of our test id card image is 400x266
 		
 	* id_type_inx -> default id == 0 , lisence id  == 1  , car plate id == 2
 
-	* returns id string    -> if succeeded 
+	* returns id_string  , True  -> if succeeded 
 
-		* returns False bool   -> if Failed
+		* returns best_guess_ID , False bool   -> if Failed
 	"""
 	#simple ids templates
 	# this is good for future if wanted to extend code to scan car plates ofقdiffز id types
@@ -416,7 +634,7 @@ def ocr_main (id_dimension : tuple = (405 , 271) , id_type_indx : int = 0 ) -> s
 
 	#needed later
 	final_value = None 
-	vid, *vid_specs = video_settings_setup()
+	vid, *vid_specs = video_settings_setup(vid_length_sec= 60)
 	active_gpu_api = vid_specs[3]
 
 	if active_gpu_api == 1 : #Cuda
@@ -441,12 +659,11 @@ def ocr_main (id_dimension : tuple = (405 , 271) , id_type_indx : int = 0 ) -> s
  
 	if final_status : 
 		id = scanned_id
-		return id 
+		return id , True
 	else : 
-		return False #FAIL
+		return  scanned_id , False #FAIL but thus is best guess
 #_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
 
 
-
 if __name__ == "__main__": 
-	ocr_main()
+	print(f"you ocr output is: {ocr_main()}")
