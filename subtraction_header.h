e0fd569746dc4f99844962f89b3586c922838c2e
Omar Rashad <77698890+orsnaro@users.noreply.github.com>
2024-03-11 00:50:49 +0200

    Delete subtraction_header.h

--- subtraction_header.h
+++
@@ -1,9 +0,0 @@
-
-
-//this belongs to ors simple calculator.
-// حطيتها لوحدها فقط لكي اقوم بتطبيق عدة طرق في تنفيذ وندا الدوال ! :P
-float subtraction(float x,float y)
-{
-float res=x-y;
-return(res);
-}
